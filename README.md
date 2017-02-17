07fd8afb909dd3e2fda6930649d77a1d2f0896fb
Costin STROIE <cstroie@bitdefender.com>
2017-02-17 18:40:10 +0200

    Removed the old LUA files.

--- README.md
+++
@@ -1,4 +0,0 @@
-# WxStation
-WiFi connected weather station, based on NodeMCU.
-
-Sensors: BME280 and TSL2561
--- init.lua
+++
@@ -1,8 +0,0 @@
--- INIT, 3 second delay before running main.lua
-local IDLE_AT_STARTUP_MS = 3
-print("Starting up in " .. IDLE_AT_STARTUP_MS .. " seconds ...")
-tmr.alarm(0, IDLE_AT_STARTUP_MS * 1000, tmr.ALARM_SINGLE, function()
-  print("Go!")
-  dofile("main.lc")
-end)
--- vim: set ft=lua ai ts=2 sts=2 et sw=2 sta nowrap nu :
